d7d08f7f601bb7925b74ac2c68de13c6b56c586d
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-01-19 18:44:45 +0600

    admin routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,23 +2,27 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
+import { adminPaths } from "./admin.routes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
+  },
+  {
+    path: "/admin",
+    element: <App />,
+    children: adminPaths,
+  },
+  {
+    path: "/faculty",
+    element: <App />,
+    children: adminPaths,
+  },
+  {
+    path: "/student",
+    element: <App />,
+    children: adminPaths,
   },
   {
     path: "/login",
