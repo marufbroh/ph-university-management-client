d0775944e4b4fd2db56420cbd68f3604d8d0b054
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-01-19 19:21:31 +0600

    working on admin routes reduce

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -3,6 +3,45 @@ import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
 
+export const adminPaths2 = [
+  {
+    name: "Dashboard",
+    path: "dashboard",
+    element: <AdminDashboard />,
+  },
+  {
+    name: "User Management",
+    children: [
+      {
+        name: "Create Admin",
+        path: "create-admin",
+        element: <CreateAdmin />,
+      },
+      {
+        name: "Create Faculty",
+        path: "create-faculty",
+        element: <CreateFaculty />,
+      },
+      {
+        name: "Create Student",
+        path: "create-student",
+        element: <CreateStudent />,
+      },
+    ],
+  },
+];
+
+const newArray = adminPaths2.reduce((acc, item) => {
+  if (item.path && item.element) {
+    acc.push({
+      path: item.path,
+      element: item.element,
+    });
+  }
+
+  if(item.children)
+}, []);
+
 export const adminPaths = [
   {
     path: "dashboard",
