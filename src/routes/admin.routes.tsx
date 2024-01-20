f67cf193024c377d4930406303cd6d02351a370c
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-01-20 07:12:22 +0600

    Refactor Routes With Util Function

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -5,18 +5,13 @@ import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
 import { ReactNode } from "react";
 
-type TRoute = {
-  path: string;
-  element: ReactNode;
-};
-
 type TSidebarItem = {
   key: string;
   label: ReactNode;
   children?: TSidebarItem[];
 };
 
-const adminPaths = [
+export const adminPaths = [
   {
     name: "Dashboard",
     path: "dashboard",
@@ -68,26 +63,6 @@ export const adminSidebarItem = adminPaths.reduce(
   },
   []
 );
-
-export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
-  if (item.path && item.element) {
-    acc.push({
-      path: item.path,
-      element: item.element,
-    });
-  }
-
-  if (item.children) {
-    item.children.forEach((child) => {
-      acc.push({
-        path: child.path,
-        element: child.element,
-      });
-    });
-  }
-
-  return acc;
-}, []);
 
 // export const adminPaths = [
 //   {
