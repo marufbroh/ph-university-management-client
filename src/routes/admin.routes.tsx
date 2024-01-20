3ee632eb4faadf5a18a91743242a28d29666aea3
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-01-20 08:06:20 +0600

    Render Sidebar Based On Role

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,15 +1,7 @@
-import { NavLink } from "react-router-dom";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
-import { ReactNode } from "react";
-
-type TSidebarItem = {
-  key: string;
-  label: ReactNode;
-  children?: TSidebarItem[];
-};
 
 export const adminPaths = [
   {
@@ -38,47 +30,3 @@ export const adminPaths = [
     ],
   },
 ];
-
-export const adminSidebarItem = adminPaths.reduce(
-  (acc: TSidebarItem[], item) => {
-    if (item.name && item.path) {
-      acc.push({
-        key: item.name,
-        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
-      });
-    }
-
-    if (item.children) {
-      acc.push({
-        key: item.name,
-        label: item.name,
-        children: item.children.map((child) => ({
-          key: child.name,
-          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
-        })),
-      });
-    }
-
-    return acc;
-  },
-  []
-);
-
-// export const adminPaths = [
-//   {
-//     path: "dashboard",
-//     element: <AdminDashboard />,
-//   },
-//   {
-//     path: "create-student",
-//     element: <CreateStudent />,
-//   },
-//   {
-//     path: "create-admin",
-//     element: <CreateAdmin />,
-//   },
-//   {
-//     path: "create-faculty",
-//     element: <CreateFaculty />,
-//   },
-// ];
