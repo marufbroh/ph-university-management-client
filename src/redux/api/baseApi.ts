404b02ca79390bc7e3cc6e2243b7f0654ca77748
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-01-21 19:01:50 +0600

    Resolve TS Error In BaseApi

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,4 @@
-
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BaseQueryApi, BaseQueryFn, DefinitionType, FetchArgs, createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 import { logout, setUser } from "../features/auth/authSlice";
 
@@ -15,33 +14,34 @@ const baseQuery = fetchBaseQuery({
     }
 });
 
-const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
-    let result = await baseQuery(args, api, extraOptions);
+const baseQueryWithRefreshToken: BaseQueryFn<FetchArgs, BaseQueryApi, DefinitionType> =
+    async (args, api, extraOptions): Promise<any> => {
+        let result = await baseQuery(args, api, extraOptions);
 
-    if (result.error?.status === 401) {
-        const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
-            method: "POST",
-            credentials: "include",
-        });
+        if (result.error?.status === 401) {
+            const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+                method: "POST",
+                credentials: "include",
+            });
 
-        const data = await res.json();
-        if (data?.data?.accessToken) {
-            const user = (api.getState() as RootState).auth.user;
-            api.dispatch(
-                setUser({
-                    user,
-                    token: data.data.accessToken
-                })
-            )
-            result = await baseQuery(args, api, extraOptions);
-        } else {
-            api.dispatch(logout())
+            const data = await res.json();
+            if (data?.data?.accessToken) {
+                const user = (api.getState() as RootState).auth.user;
+                api.dispatch(
+                    setUser({
+                        user,
+                        token: data.data.accessToken
+                    })
+                )
+                result = await baseQuery(args, api, extraOptions);
+            } else {
+                api.dispatch(logout())
+            }
+
         }
 
+        return result;
     }
-
-    return result;
-}
 
 export const baseApi = createApi({
     reducerPath: "baseApi",
