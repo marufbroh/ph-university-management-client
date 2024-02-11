dd4336a73510285aab8036035bcb211fd7d362d6
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-02-11 12:17:04 +0600

    Add Academic Semester Using RTK Query Mutation

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -6,6 +6,8 @@ import PHSelect from "../../../components/form/PHSelect";
 import { monthOptions } from "../../../constants/global";
 import { semesterOptions } from "../../../constants/semester";
 import { academicSemesterSchema } from "../../../schemas/academicManagement.schema";
+import { useAddAcademicSemestersMutation } from "../../../redux/features/admin/academicManagement.api";
+import { toast } from "sonner";
 
 const currentYear = new Date().getFullYear();
 const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
@@ -14,7 +16,9 @@ const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
 }));
 
 const CreateAcademicSemester = () => {
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+  const [addAcademicSemester] = useAddAcademicSemestersMutation();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     const name = semesterOptions[Number(data?.name) - 1]?.label;
 
     const semesterData = {
@@ -24,7 +28,13 @@ const CreateAcademicSemester = () => {
       startMonth: data.startMonth,
       endMonth: data.endMonth,
     };
-    console.log(semesterData);
+
+    try {
+      const res = await addAcademicSemester(semesterData);
+      console.log(res);
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
   };
 
   return (
--- src/redux/features/admin/academicManagement.api.ts
+++ src/redux/features/admin/academicManagement.api.ts
@@ -7,8 +7,15 @@ const academicManagementApi = baseApi.injectEndpoints({
                 url: "/academic-semesters",
                 method: "GET"
             })
-        })
+        }),
+        addAcademicSemesters: builder.mutation({
+            query: (data) => ({
+                url: "/academic-semesters/create-academic-semester",
+                method: "POST",
+                body: data
+            })
+        }),
     }),
 });
 
-export const {useGetAllSemestersQuery} = academicManagementApi
+export const {useGetAllSemestersQuery, useAddAcademicSemestersMutation} = academicManagementApi
