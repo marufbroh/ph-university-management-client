57233ae41cc983aa91adc6c8e31cccad3710f222
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-02-11 19:17:46 +0600

    Module Wrap Up And Tasks

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,12 +1,13 @@
 import type { TableColumnsType, TableProps } from "antd";
-import { Table } from "antd";
+import { Button, Progress, Table } from "antd";
 import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.api";
 import { TAcademicSemester } from "../../../types/academicManagement.type";
 import { useState } from "react";
+import { TQueryParam } from "../../../types";
 
 export type TTableData = Pick<
   TAcademicSemester,
-  "_id" | "name" | "startMonth" | "endMonth" | "year"
+  "name" | "startMonth" | "endMonth" | "year"
 >;
 
 const columns: TableColumnsType<TTableData> = [
@@ -44,11 +45,26 @@ const columns: TableColumnsType<TTableData> = [
     key: "endMonth",
     dataIndex: "endMonth",
   },
+  {
+    title: "Action",
+    key: "x",
+    render: () => {
+      return (
+        <div>
+          <Button>Update</Button>
+        </div>
+      );
+    },
+  },
 ];
 
 const AcademicSemester = () => {
-  const [params, setParams] = useState([]);
-  const { data: semesterData } = useGetAllSemestersQuery(params);
+  const [params, setParams] = useState<TQueryParam[] | undefined>(undefined);
+  const {
+    data: semesterData,
+    isLoading,
+    isFetching,
+  } = useGetAllSemestersQuery(params);
 
   const tableData = semesterData?.data?.map(
     ({ _id, name, startMonth, endMonth, year }) => ({
@@ -61,23 +77,35 @@ const AcademicSemester = () => {
   );
 
   const onChange: TableProps<TTableData>["onChange"] = (
-    pagination,
+    _pagination,
     filters,
-    sorter,
+    _sorter,
     extra
   ) => {
     if (extra.action === "filter") {
-      const queryParams: any = [];
+      const queryParams: TQueryParam[] = [];
       filters.name?.forEach((item) => {
         queryParams.push({ name: "name", value: item });
       });
-      setParams(queryParams)
+      setParams(queryParams);
     }
     console.log({ filters, extra });
   };
 
+  if (isLoading) {
+    const twoColors = { "0%": "#108ee9", "100%": "#87d068" };
+    return <Progress percent={99.9} strokeColor={twoColors} />;
+  }
+
   //   console.log(tableData);
-  return <Table columns={columns} dataSource={tableData} onChange={onChange} />;
+  return (
+    <Table
+      loading={isFetching}
+      columns={columns}
+      dataSource={tableData}
+      onChange={onChange}
+    />
+  );
 };
 
 export default AcademicSemester;
